c4c98442ad873df7957ec0389d2ac8e4ac49dfdf
Evgeniy Kukushkin <eugin86@list.ru>
2022-09-20 21:13:35 +0500

    добавлена Задача №2 - домашнего задания

---
+++ task-06-homework-02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Программа выдачи результата сравнения двух чисел.\r\n");
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} - большее, число {number2} - меньшее");
+}
+else
+{
+    Console.WriteLine($"Число {number2} - большее, число {number1} - меньшее");
+}
