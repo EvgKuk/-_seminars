d531fd6245e6b8bce7233321781991b8c88f663e
Evgeniy Kukushkin <eugin86@list.ru>
2022-09-20 21:11:45 +0500

    добавлена третья задача с семинара

---
+++ task-03/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет выдавать название
+// дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Программа выдачи названия дня недели по номеру.\r\n");
+Console.Write("Введите номер дня недели (целое число от 1 до 7): ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+switch (numberDay)
+{
+    case 1:
+        Console.Write($"День номер {numberDay} - это понедельник");
+        break;
+    case 2:
+        Console.Write($"День номер {numberDay} - это вторник");
+        break;
+    case 3:
+        Console.Write($"День номер {numberDay} - это среда");
+        break;
+    case 4:
+        Console.Write($"День номер {numberDay} - это четверг");
+        break;
+    case 5:
+        Console.Write($"День номер {numberDay} - это пятница");
+        break;
+    case 6:
+        Console.Write($"День номер {numberDay} - это суббота");
+        break;
+    case 7:
+        Console.Write($"День номер {numberDay} - это воскресенье");
+        break;
+    default:
+        Console.Write($"Ошибка!  Названия введённого номера дня не существует!");
+        break;
+}
