f8a047265cf72d706feb906b310ceba74c883067
Evgeniy Kukushkin <eugin86@list.ru>
2022-09-20 21:14:00 +0500

    добавлена Задача №4 - домашнего задания

---
+++ task-07-homework-04/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Программа выдачи результата сравнения трёх чисел.\r\n");
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 & number1 > number3)
+{
+    Console.WriteLine($"Максимальное число - {number1}");
+}
+else if (number2 > number1 & number2 > number3)
+{
+    Console.WriteLine($"Максимальное число - {number2}"); 
+}
+else
+{
+    Console.WriteLine($"Максимальное число - {number3}");
+}
