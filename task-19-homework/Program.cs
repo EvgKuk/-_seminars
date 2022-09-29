619e4f71ac29f58f34b46e836966f6185b7a7944
Evgeniy Kukushkin <eugin86@list.ru>
2022-09-30 00:19:04 +0500

    добавлены задачи с 19 по 27

---
+++ task-19-homework/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19
+// Напишите программу, которая принимает
+// на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Программа определения является ли введённое число палиндромом.\r\n");
+Console.WriteLine("");
+Console.Write("Введите число: ");
+int numPal = Convert.ToInt32(Console.ReadLine());
+if (numPal < 0)
+{
+    Console.WriteLine("Число меньше ноля");
+}
+int tempNumPal = numPal;
+int value;
+int numRev = 0;
+
+while (tempNumPal > 0)
+{
+    value = tempNumPal % 10;
+    numRev = numRev*10 + value;
+    tempNumPal = tempNumPal / 2;
+}
+
+if (numRev == numPal)
+{
+    Console.WriteLine("Введённое чило палиндром");
+}
+else
+{
+Console.WriteLine("Введённое чило палиндром");
+}
--- task-27-homework/Program.cs
+++ task-27-homework/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая
+// принимает на вход число и выдаёт
+// сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Программа определения суммы цифр числа.\r\n");
+Console.WriteLine("");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberSumm(int number){
+    
+    int count = Convert.ToString(number).Length;
+    int progression = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      progression = number - number % 10;
+      result = result + (number - progression);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int numberSumm = NumberSumm(number);
+Console.WriteLine("Сумма цифр числа: " + numberSumm);
