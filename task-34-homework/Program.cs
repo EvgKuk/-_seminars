d5fe3534998cb0c474184de5e0b71e6106d3ce88
Evgeniy Kukushkin <eugin86@list.ru>
2022-10-04 23:23:40 +0500

    добавлено решение задачи 34

--- task-34-homework/Program.cs
+++ task-34-homework/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный
+// случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет
+// количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Программа вывода чётных чисел массива.\r\n");
+Console.WriteLine("");
+
+Console.Write("Введите размер массива (size): ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = new int[size];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(); 
+    Console.WriteLine($"{array[i]}");  
+}
+
+
+void EvenMethod(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            even++;    
+        }
+        
+    }
+    Console.WriteLine($"Количество чётных чисел: {even}");
+}
+
+EvenMethod(array);
