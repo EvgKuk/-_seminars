57017597c8a18585f74766b520a20d86f7ed37f8
Evgeniy Kukushkin <eugin86@list.ru>
2022-10-09 20:29:58 +0500

    добавлены задачи 34, 36, 38

--- task-36-homework/Program.cs
+++ task-36-homework/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив,
+// заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+Console.Write("Программа вывода суммы элементов на нечётных позициях.\r\n");
+Console.WriteLine("");
+
+Console.Write("Введите размер массива (size): ");
+
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+Random random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(size); 
+    Console.Write($"{array[i]} / ");
+}
+Console.WriteLine(""); 
+
+
+void OddSumMethod(int[] array)
+{
+    int oddSum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            oddSum += array[i];    
+        }
+        
+    }
+    Console.WriteLine($"Сумма нечётных элементов {oddSum}");
+}
+
+OddSumMethod(array);
--- task-38-homework/Program.cs
+++ task-38-homework/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.Write("Программа поиска разницы макс и мин вещественного числа массива.\r\n");
+Console.WriteLine("");
+
+Console.Write("Введите размер массива (size): ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+
+Console.Write("Введите делитель 100, 1000, 10000 и т.п.  (del): ");
+int del = Convert.ToInt32(Console.ReadLine());
+
+double minVal = Int32.MaxValue;
+double maxVal = Int32.MinValue;
+
+fillRandom(array);
+minMaxDoubleMethod(array);
+
+void fillRandom(double[] array)
+{
+    Random random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(random.Next(size)) / 100; 
+            Console.Write($"{array[i]} / ");
+            
+        }
+        Console.WriteLine("");
+}
+
+void minMaxDoubleMethod(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxVal)
+        {
+            maxVal = array[i];
+        }
+        if (array[i] < minVal)
+        {
+            minVal = array[i];
+        }
+    }
+Console.WriteLine($"Макс значение = {maxVal}, мин значение = {minVal}");
+Console.WriteLine("");
+Console.WriteLine($"Разница между макс и мин значением = {maxVal - minVal}");
+}
+
