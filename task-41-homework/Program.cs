cb886b08ac1ea1f3fb0cc037c795f69d44aeb352
Evgeniy Kukushkin <eugin86@list.ru>
2022-10-10 22:57:02 +0500

    добавлено решение по задачам 41 и 43

--- task-41-homework/Program.cs
+++ task-41-homework/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Программа подсчёта введённых чисел больше ноля.\r\n");
+Console.WriteLine("");
+
+Console.Write("Введите целые числа, через запятую или через точку: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(',' , '.'), int.Parse);
+int count = 0;
+
+FillNumbers(array);
+
+void FillNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine($"Пользователь ввёл {count} элементов больше ноля");
